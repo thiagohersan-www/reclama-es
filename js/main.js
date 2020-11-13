da8608aae8be1da7bca561c1659964e608732a27
Thiago Hersan <thiago.hersan+github@gmail.com>
2020-11-13 01:23:28 -0300

    reworking all html/js

--- js/main.js
+++ js/main.js
@@ -1,71 +1,67 @@
-var apiUrl = "https://complain-gg.herokuapp.com/api/complain/";
+let complaintCount;
+const fontFamily = ['Patrick Hand', 'Kalam', 'Shadows Into Light Two'];
 
-var QUEUE_INTERVAL = 10000;
-var SERVER_INTERVAL = 30000;
+const TEXT_PT = '<span style="font-style:italic;">Complaints Department</span> [Departamento de reclamações] (2016 - ) é um projeto colaborativo das <a href="http://www.guerrillagirls.com/" target="_blank">Guerrilla Girls</a>. Inicialmente realizado na Tate Modern, em Londres, ele agora ganhou uma nova versão para a segunda <a href="http://frestas.sescsp.org.br/" target="_blank">Frestas - Trienal de Artes</a>. Fique à vontade para expressar aqui suas queixas, incômodos e críticas. A partir de 12 de agosto a obra também pode ser acessada no Centro de convivência do Sesc Sorocaba.';
 
-var newestId;
-var oldestId;
-var complaintQueue = [];
-var newComplaintsIntervalHandler;
-var complaintCount;
-var fontFamily = ['Patrick Hand', 'Kalam', 'Shadows Into Light Two'];
+const TEXT_EN = '<span style="font-style:italic;">Complaints Department</span> [Departamento de reclamações] (2016 - ) is a collaborative project by the <a href="http://www.guerrillagirls.com/" target="_blank">Guerrilla Girls</a>. Initially created for the Tate Modern in London, a new version has been prepared for the second edition of <a href="http://frestas.sescsp.org.br/" target="_blank">Frestas - Triennial of Arts</a>. Feel free to send in all your complaints, pet peeves and criticisms. Starting on August 12, the work will also be available for access at the Sesc Sorocaba Recreational Center.';
 
-var TEXT_PT = '<span style="font-style:italic;">Complaints Department</span> [Departamento de reclamações] (2016 - ) é um projeto colaborativo das <a href="http://www.guerrillagirls.com/" target="_blank">Guerrilla Girls</a>. Inicialmente realizado na Tate Modern, em Londres, ele agora ganhou uma nova versão para a segunda <a href="http://frestas.sescsp.org.br/" target="_blank">Frestas - Trienal de Artes</a>. Fique à vontade para expressar aqui suas queixas, incômodos e críticas. A partir de 12 de agosto a obra também pode ser acessada no Centro de convivência do Sesc Sorocaba.';
+const enButton = document.getElementById('language-button-en');
+const ptButton = document.getElementById('language-button-pt');
+const sendButton = document.getElementById('send-button');
+const complaintInput = document.getElementById('input-complaint');
+const complaintLightbox = document.getElementById('complaint-lightbox');
+const complaintHighlight = document.getElementById('complaint-container-highlight');
+const highlightClose = document.getElementById('highlight-close-button');
+const introText = document.getElementById('intro-text');
 
-var TEXT_EN = '<span style="font-style:italic;">Complaints Department</span> [Departamento de reclamações] (2016 - ) is a collaborative project by the <a href="http://www.guerrillagirls.com/" target="_blank">Guerrilla Girls</a>. Initially created for the Tate Modern in London, a new version has been prepared for the second edition of <a href="http://frestas.sescsp.org.br/" target="_blank">Frestas - Triennial of Arts</a>. Feel free to send in all your complaints, pet peeves and criticisms. Starting on August 12, the work will also be available for access at the Sesc Sorocaba Recreational Center.';
 
 window.onload = function() {
   setClicks();
   complaintCount = 0;
-  getOldComplaints();
-  newComplaintsIntervalHandler = setInterval(getNewComplaints, SERVER_INTERVAL);
 };
 
 function setClicks() {
-  document.getElementById('complaint-lightbox').onclick = closeLightbox;
-  document.getElementById('highlight-close-button').onclick = closeLightbox;
-  document.getElementById('language-button-pt').onclick = textToPortuguese;
-  document.getElementById('language-button-en').onclick = textToEnglish;
-  document.getElementById('complaint-container-highlight').onclick = doNothing;
-  document.getElementById('send-button').onclick = postNewComplaint;
-  document.getElementById('load-button').onclick = getOldComplaints;
+  highlightClose.onclick = closeLightbox;
+  complaintHighlight.onclick = doNothing;
+  complaintLightbox.onclick = closeLightbox;
+  ptButton.onclick = textToPortuguese;
+  enButton.onclick = textToEnglish;
+  sendButton.onclick = addComplaint;
 }
 
 function textToEnglish() {
-  document.getElementById('intro-text').innerHTML = TEXT_EN;
-  document.getElementById('language-button-en').classList.remove('language-button-clean');
-  document.getElementById('language-button-en').classList.add('language-button-selected');
-  document.getElementById('language-button-pt').classList.remove('language-button-selected');
-  document.getElementById('language-button-pt').classList.add('language-button-clean');
-  document.getElementById('input-complaint').setAttribute('placeholder', 'Complain here');
-  document.getElementById('send-button').setAttribute('value', 'send');
-  document.getElementById('load-button').innerHTML = 'more complaints';
+  introText.innerHTML = TEXT_EN;
+  enButton.classList.remove('language-button-clean');
+  enButton.classList.add('language-button-selected');
+  ptButton.classList.remove('language-button-selected');
+  ptButton.classList.add('language-button-clean');
+  complaintInput.setAttribute('placeholder', 'Complain here');
+  sendButton.setAttribute('value', 'send');
 }
 
 function textToPortuguese() {
-  document.getElementById('intro-text').innerHTML = TEXT_PT;
-  document.getElementById('language-button-pt').classList.remove('language-button-clean');
-  document.getElementById('language-button-pt').classList.add('language-button-selected');
-  document.getElementById('language-button-en').classList.remove('language-button-selected');
-  document.getElementById('language-button-en').classList.add('language-button-clean');
-  document.getElementById('input-complaint').setAttribute('placeholder', 'Escreva aqui sua reclamação');
-  document.getElementById('send-button').setAttribute('value', 'enviar');
-  document.getElementById('load-button').innerHTML = 'mais reclamações';
+  introText.innerHTML = TEXT_PT;
+  ptButton.classList.remove('language-button-clean');
+  ptButton.classList.add('language-button-selected');
+  enButton.classList.remove('language-button-selected');
+  enButton.classList.add('language-button-clean');
+  complaintInput.setAttribute('placeholder', 'Escreva aqui sua reclamação');
+  sendButton.setAttribute('value', 'enviar');
+}
+
+function setOpacity(element, opacity, delay) {
+  setTimeout(() => element.style.opacity = opacity, delay);
 }
 
 window.onscroll = function() {
-  var introTextYLocation = document.getElementById('complaint-form').offsetTop - 256;
-
-  if(window.pageYOffset > introTextYLocation) {
-    document.getElementById('desktop-share-container').style.top = '128px';
-  } else {
-    document.getElementById('desktop-share-container').style.top = '-128px';
-  }
+  const introTextYLocation = document.getElementById('complaint-form').offsetTop - 256;
+  const shareContainer = document.getElementById('desktop-share-container');
+  shareContainer.style.top = (window.pageYOffset > introTextYLocation) ? '128px' : '-128px';
 }
 
 function randomRange(a, b) {
-  var min = Math.min(a, b);
-  var max = Math.max(a, b);
+  const min = Math.min(a, b);
+  const max = Math.max(a, b);
   return (max - min) * Math.random() + min;
 }
 
@@ -74,99 +70,54 @@ function doNothing() {
 }
 
 function closeLightbox() {
-  var lightbox = document.getElementById('complaint-lightbox');
-  lightbox.style.opacity = '0';
-  setTimeout(function() {
-    var lightbox = document.getElementById('complaint-lightbox');
-    lightbox.style.display = 'none';
-  }, 500);
+  complaintLightbox.style.opacity = '0';
+  setTimeout(() => complaintLightbox.style.display = 'none', 500);
 }
 
 function showLightbox(complaint, colorClass, complaintFont) {
-  var lightBox = document.getElementById('complaint-lightbox');
+  complaintHighlight.classList = '';
+  complaintHighlight.classList.add('complaint-container');
+  complaintHighlight.classList.add(colorClass);
 
-  var highlightElement = document.getElementById('complaint-container-highlight');
-  highlightElement.classList = "";
-  highlightElement.classList.add('complaint-container');
-  highlightElement.classList.add(colorClass);
-
-  var highlightTextElement = highlightElement.getElementsByClassName('complaint-text')[0];
-  highlightTextElement.classList = "";
+  const highlightTextElement = complaintHighlight.getElementsByClassName('complaint-text')[0];
+  highlightTextElement.classList = '';
   highlightTextElement.classList.add('complaint-text');
   highlightTextElement.innerHTML = complaint;
   highlightTextElement.style['font-family'] = complaintFont;
   highlightTextElement.style['font-size'] = '';
   if(complaint.length < 120) {
-    highlightTextElement.style['font-size'] = Math.max((100-complaint.length)/14, 3.3)+"em";
+    highlightTextElement.style['font-size'] = Math.max((100-complaint.length)/14, 3.3) + 'em';
   } else if(complaint.length < 180) {
     highlightTextElement.classList.add('complaint-text-size-medium');
   } else {
     highlightTextElement.classList.add('complaint-text-size-small');
   }
 
-  lightBox.style.display = 'block';
-  setTimeout(function() {
-    var lightbox = document.getElementById('complaint-lightbox');
-    lightBox.style.opacity = '1';
-  }, 1);
-}
-
-function getOldComplaints() {
-  var formData = {};
-  if(oldestId) {
-    formData.last = oldestId;
-  }
-  jsonPostGet({url: apiUrl+'last/', data: JSON.stringify(formData)}, populateList);
-}
-
-function getNewComplaints() {
-  if(complaintQueue.length > 0) return;
-  var formData = {};
-  if(newestId) {
-    formData.since = newestId;
-  }
-  jsonPostGet({url: apiUrl+'since/', data: JSON.stringify(formData)}, handleNewComplaints);
+  complaintLightbox.style.display = 'block';
+  setTimeout(() => complaintLightbox.style.opacity = '1', 1);
 }
 
 function postNewComplaint() {
-  var formData = {};
-  formData.complaint = document.getElementById('input-complaint').value;
-
-  if(formData.complaint.replace(/ /g, '') != '') {
-    jsonPostGet({url: apiUrl, data: JSON.stringify(formData)}, handleNewComplaints);
-  }
-}
-
-function processQueue() {
-  if(complaintQueue.length < 1) {
-    newComplaintsIntervalHandler = setInterval(getNewComplaints, SERVER_INTERVAL);
-  } else {
-    clearInterval(newComplaintsIntervalHandler);
-    var firstComplaint = complaintQueue.shift();
-    addComplaint(firstComplaint);
-    setTimeout(processQueue, QUEUE_INTERVAL);
-  }
+  addComplaint();
 }
 
 function createComplaintElement(complaint) {
-  var complaintP = document.createElement('p');
-  var complaintDiv = document.createElement('div');
+  const complaintP = document.createElement('p');
+  const complaintDiv = document.createElement('div');
 
   complaintP.innerHTML = complaint.complaint;
 
-  complaintP.classList.add("complaint-text");
-  complaintP.style['font-size'] = Math.max((80-complaint.complaint.length)/14, 2.0)+"em";
+  complaintP.classList.add('complaint-text');
+  complaintP.style['font-size'] = Math.max((80-complaint.complaint.length)/14, 2.0) + 'em';
   complaintP.style['font-family'] = fontFamily[complaintCount % 3];
 
-  var colorClass = "complaint-container-color-"+(complaintCount % 4);
-  complaintDiv.classList.add("complaint-container");
+  const colorClass = `complaint-container-color-${complaintCount % 4}`;
+  complaintDiv.classList.add('complaint-container');
   complaintDiv.classList.add(colorClass);
-  complaintDiv.style.transform = 'rotate('+randomRange(-10, 10)+'deg)';
-  complaintDiv.onclick = (function(text, color, font) {
-    return function() {
-      showLightbox(text, color, font);
-    }
-  })(complaint.complaint, colorClass, fontFamily[complaintCount % 3]);
+  complaintDiv.style.transform = `rotate(${randomRange(-10, 10)}deg)`;
+  complaintDiv.onclick = (() => {
+    showLightbox(complaint.complaint, colorClass, fontFamily[complaintCount % 3]);
+  });
 
   complaintCount++;
 
@@ -175,45 +126,10 @@ function createComplaintElement(complaint) {
   return complaintDiv;
 }
 
-function addComplaint(complaint) {
-  var complaintElement = createComplaintElement(complaint);
-  var listDiv = document.getElementById('complaint-list');
+function addComplaint() {
+  if (complaintInput.value.replace(/ /g, '').length < 1) return;
+  const complaintElement = createComplaintElement({ complaint: complaintInput.value });
+  const listDiv = document.getElementById('complaint-list');
   listDiv.insertBefore(complaintElement, listDiv.firstChild);
   setOpacity(complaintElement, '1', 100);
-}
-
-function handleNewComplaints(complaints) {
-  if(complaints.length < 1) return;
-  newestId = complaints[complaints.length - 1]._id;
-  oldestId = oldestId || complaints[0]._id;
-
-  var firstComplaint = complaints.shift();
-  addComplaint(firstComplaint);
-
-  for(var i in complaints) {
-    complaintQueue.push(complaints[i]);
-  }
-  setTimeout(processQueue, QUEUE_INTERVAL);
-}
-
-function setOpacity(element, opacity, delay) {
-  setTimeout(function() {
-    element.style.opacity = opacity;
-  }, delay);
-}
-
-function populateList(complaints) {
-  if(complaints.length < 1) return;
-  newestId = newestId || complaints[0]._id;
-  oldestId = complaints[complaints.length - 1]._id;
-
-  document.getElementById('complaints-loader-spinner').style.display = 'block';
-  var listDiv = document.getElementById('complaint-list');
-
-  for(var i in complaints) {
-    var newElement = createComplaintElement(complaints[i]);
-    listDiv.appendChild(newElement);
-    setOpacity(newElement, '1', 100*i);
-  }
-  document.getElementById('complaints-loader-spinner').style.display = 'none';
 }
